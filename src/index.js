0530ec0a7a6ab4c39e2bebe181af2fe7a51adfd2
orlov-dm <dm.ed.orlov@gmail.com>
2018-09-29 20:05:33 +0300

    feat: Dropdown instancing

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 import "@babel/polyfill";
-import Dropdown from './Dropdown';
 import 'normalize.css';
+import { stringify } from 'svgson-next';
+
+import Dropdown from './Dropdown';
 import './styles/main.css';
 
-import Avatars from '@dicebear/avatars';
-import MaleSprites from '@dicebear/avatars-male-sprites';
-import FemaleSprites from '@dicebear/avatars-female-sprites';
-
-const MaleAvatars = new Avatars(MaleSprites);
-const FemaleAvatars = new Avatars(FemaleSprites);
-
-(new Dropdown).hello();
 
 
 function getData() {
     // Read the JSON-formatted data from the DOM.
     const element = document.getElementById('user_data');
-    const data = JSON.parse(element.textContent);
+    const data = JSON.parse(element.textContent, function(key, value) {
+        if(key === 'avatarUrl') {
+            return stringify(value);
+        }
+        return value;
+    });
     // Clear the element’s contents now that we have a copy of the data.
-    element.innerHTML = '';
+    element.remove();
     return data;
 };
 
 const data = getData();
-console.log(data);
 
-document.getElementById('root').addEventListener('click', function() {
-
-    let svg = avatars.create('custom-seed1');
-    document.getElementById('avatar').innerHTML = svg;
-});
+(new Dropdown({ id: 'dropdown', data })).render();
